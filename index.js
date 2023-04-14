7ada6a494967f35a66c61f3a8df2fe0ad90826ec
footballjones <footballjones308@gmail.com>
2023-04-14 16:12:50 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const userInfo = {
+        name: `${userName}`,
+        email: `${userEmail}`,
+    }
+    
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+    },
+        body: JSON.stringify(userInfo)
+    
+    
+}
+
+    return fetch(`http:localhost:3000/users`, configurationObject)
+        .then(response => response.json())
+        .then(object => console.log(object))
+        .catch(function(error) {
+        alert("Error")
+        console.log(error)
+  })
+}
+submitData("John", "kenaa@example.com")
